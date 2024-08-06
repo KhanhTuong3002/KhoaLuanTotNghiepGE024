bb4100cdfffc792e67f784e4657424a2cad0576a
NamCE170031 <namdnce170031@fpt.edu.vn>
2024-08-06 18:24:37 +0700

    fix set id return null, update message

--- GeoTycoon/Assets/Scripts/MainMenu/SetValidator.cs
+++ GeoTycoon/Assets/Scripts/MainMenu/SetValidator.cs
@@ -37,20 +37,43 @@ public class SetValidator : MonoBehaviourPunCallbacks
     {
         if(PhotonNetwork.IsConnected)
         {
-            if(isDataFound) WarningMessageOnl.gameObject.SetActive(false);   
-            else WarningMessageOnl.gameObject.SetActive(true);   
+            if(isDataFound) WarningMessageOnl.text = "<color=green>Question pack found!</color>"; 
+            else WarningMessageOnl.text = "<color=red>Question pack not found</color>";
+
+            WarningMessageOnl.gameObject.SetActive(true);   
         }
         else
         {
-            if(isDataFound) WarningMessageOff.gameObject.SetActive(false);   
-            else WarningMessageOff.gameObject.SetActive(true);   
+            if(isDataFound) WarningMessageOff.text = "<color=green>Question pack found!</color>";
+            else WarningMessageOff.text = "<color=red>Question pack not found</color>";
+
+            WarningMessageOff.gameObject.SetActive(true);   
         }
+        StartCoroutine(DisableWarning());
         return isDataFound;
     }
 
+    public IEnumerator DisableWarning()
+    {
+        yield return new WaitForSeconds(1.5f);
+        WarningMessageOnl.gameObject.SetActive(false);   
+        WarningMessageOff.gameObject.SetActive(false); 
+    }
 
+//<b><color=red>has to stay in Jail</color></b>
     public void GetData(string setID)
     {
+        if(PhotonNetwork.IsConnected)
+        {
+            WarningMessageOnl.text = "Searching...";
+            WarningMessageOnl.gameObject.SetActive(true);   
+        }
+        else
+        {
+            WarningMessageOff.text = "Searching...";
+            WarningMessageOff.gameObject.SetActive(true);   
+        }
+        
         if(setID=="") setID = "ec74b952-469c-4fc4-a175-498776ac12e3";
         StartCoroutine(FetchData(setID));
         Debug.Log("****QuizManager receive your SetID:**** " + setID);
@@ -65,14 +88,16 @@ public class SetValidator : MonoBehaviourPunCallbacks
         {
             yield return request.SendWebRequest();
             Debug.Log("Searching...");
-            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError || request.downloadHandler.text.Length < 3)
             {
+                Debug.Log(request.downloadHandler.text);
                 Debug.Log(request.error);
                 Debug.Log("Set not found");
                 SetIsValid(false);
             }
             else
             {
+                Debug.Log(request.downloadHandler.text);
                 Debug.Log("Set found");
                 isDataFound = true;
                 SetIsValid(true);
